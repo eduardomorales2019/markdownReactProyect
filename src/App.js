fa85b786ca1db846cbb3be7b4a1d2418670b7771
EduardoMorales <eduardo@inmo-puebla.com>
2022-09-03 23:01:52 -0500

    se  termini la funcionalidad y se subio la libreria markdown..
    -

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./index.css";
+import React from "react";
+import ReactMarkDown from "react-markdown";
 
 function App() {
+  const [markdown, setMarkDown] = React.useState("## Markdown Review");
+
   return (
     <main>
       <section className="markdown">
-        <textarea className="input"></textarea>
+        <textarea
+          className="input"
+          input="text"
+          value={markdown}
+          onChange={(e) => setMarkDown(e.target.value)}
+        ></textarea>
+
+        <article className="result">
+          <ReactMarkDown>{markdown}</ReactMarkDown>
+        </article>
       </section>
     </main>
   );
